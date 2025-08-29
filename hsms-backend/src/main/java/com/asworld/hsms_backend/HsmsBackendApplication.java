06cd8bb1268d8bdc975a5c38c6bc10a359d2e462
arpit <72805524+arpit164@users.noreply.github.com>
2025-08-29 18:25:41 +0530

    1.1 - Initialized the backend project

---
+++ hsms-backend/src/main/java/com/asworld/hsms_backend/HsmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.asworld.hsms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsmsBackendApplication.class, args);
+	}
+
+}
---
+++ hsms-backend/src/test/java/com/asworld/hsms_backend/HsmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asworld.hsms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
